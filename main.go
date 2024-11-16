5ffda6da3e8b97e08985f408d533f30f0ddc5d89
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2024-11-15 19:59:14 -0600

    reporte tests ui

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"super-descuentos/rest"
 	"super-descuentos/store"
 )
@@ -11,8 +12,21 @@ func main() {
 	store := store.NewInMemoryStore()
 	server := rest.NewServer(store)
 
-	fmt.Println("Servidor corriendo en http://localhost:8080")
-	err := http.ListenAndServe(":8080", server)
+	// Get port from environment variable or use default
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	// Disable automatic HTTPS redirect in test environment
+	if os.Getenv("TEST_ENV") == "true" {
+		http.DefaultServeMux = http.NewServeMux()
+	}
+
+	addr := fmt.Sprintf(":%s", port)
+	fmt.Printf("Servidor corriendo en http://localhost%s\n", addr)
+
+	err := http.ListenAndServe(addr, server)
 	if err != nil {
 		fmt.Println(err)
 	}
